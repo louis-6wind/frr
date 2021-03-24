c20d5658fd75169c9cc861c0fcd851436db06821
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-03-24 09:40:46 -0300

    staticd: add command to show monitored routes
    Add command to visualize all monitored routes and their state.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- staticd/static_vty.c
+++ staticd/static_vty.c
@@ -1624,6 +1624,14 @@ DEFPY_YANG(debug_staticd, debug_staticd_cmd,
 	return CMD_SUCCESS;
 }
 
+DEFPY(staticd_show_bfd_routes, staticd_show_bfd_routes_cmd,
+      "show bfd static route [json]$isjson",
+      SHOW_STR BFD_INTEGRATION_STR STATICD_STR ROUTE_STR JSON_STR)
+{
+	static_bfd_show(vty, !!isjson);
+	return CMD_SUCCESS;
+}
+
 DEFUN_NOSH (show_debugging_static,
 	    show_debugging_static_cmd,
 	    "show debugging [static]",
@@ -1670,4 +1678,6 @@ void static_vty_init(void)
 	install_element(ENABLE_NODE, &show_debugging_static_cmd);
 	install_element(ENABLE_NODE, &debug_staticd_cmd);
 	install_element(CONFIG_NODE, &debug_staticd_cmd);
+
+	install_element(ENABLE_NODE, &staticd_show_bfd_routes_cmd);
 }
