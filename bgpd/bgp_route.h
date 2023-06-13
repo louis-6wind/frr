fb0002c0d452c138cd40bb2ee0ad8d31c9742afd
Alexander Sohn <github@asohn.de>
2023-06-13 10:03:33 +0200

    bgpd: add bgp_show_table_rtc
    Signed-off-by: Alexander Sohn <github@asohn.de>

--- bgpd/bgp_route.h
+++ bgpd/bgp_route.h
@@ -969,6 +969,8 @@ extern void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_de
 				 const struct prefix *p, struct bgp_path_info *path, afi_t afi,
 				 safi_t safi, enum rpki_states, json_object *json_paths,
 				 struct attr *attr);
+extern int bgp_show_table_rtc(struct vty *vty, struct bgp *bgp, safi_t safi, struct bgp_table *table,
+			      enum bgp_show_type type, void *output_arg, uint16_t show_flags);
 extern int bgp_show_table_rd(struct vty *vty, struct bgp *bgp, afi_t afi, safi_t safi,
 			     struct bgp_table *table, struct prefix_rd *prd,
 			     enum bgp_show_type type, void *output_arg,
