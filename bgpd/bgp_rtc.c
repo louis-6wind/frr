652f921fd591bdacea446360fa498795fd58a61c
Louis Scalbert <louis.scalbert@6wind.com>
2024-12-13 11:32:52 +0100

    WIP fixes from Alexander
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_rtc.c
+++ bgpd/bgp_rtc.c
@@ -18,6 +18,7 @@ int bgp_nlri_parse_rtc(struct peer *peer, struct attr *attr, struct bgp_nlri *pa
 		struct prefix p = { 0 };
 
 		p.prefixlen = *pnt++;
+		/* TODO: Correctly handle prefixlen == 0 */
 		if (p.prefixlen > BGP_RTC_MAX_PREFIXLEN || p.prefixlen < 32) {
 			zlog_err("SAFI_RTC parse error. Invalid prefixlen: %u", p.prefixlen);
 			return BGP_NLRI_PARSE_ERROR;
@@ -103,87 +104,92 @@ int bgp_rtc_filter(struct peer *peer, struct attr *attr, const struct prefix *p)
 	return false;
 }
 
-int bgp_rtc_static_from_str(struct vty *vty, struct bgp *bgp, const char *str, bool add)
+
+static void bgp_rtc_add_static(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
 {
-	struct ecommunity *ecom = NULL;
-	int plen = BGP_RTC_MAX_PREFIXLEN;
-	char *pnt;
-	char *cp;
-
-	/* Find slash inside string. */
-	pnt = strchr(str, '/');
-
-	/* String doesn't contain slash. */
-	if (pnt == NULL) {
-		ecom = ecommunity_str2com(str, ECOMMUNITY_ROUTE_TARGET, 0);
-		if (ecom == NULL) {
-			vty_out(vty, "%% Can't parse ecommunity %s\n", str);
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-	} else {
-		cp = XMALLOC(MTYPE_TMP, (pnt - str) + 1);
-		memcpy(cp, str, pnt - str);
-		*(cp + (pnt - str)) = '\0';
-		ecom = ecommunity_str2com(cp, ECOMMUNITY_ROUTE_TARGET, 0);
-
-		XFREE(MTYPE_TMP, cp);
-
-		if (ecom == NULL) {
-			vty_out(vty, "%% Can't parse ecommunity %s\n", str);
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-
-		/* Get prefix length. */
-		plen = (uint8_t)atoi(++pnt);
-		if (plen > BGP_RTC_MAX_PREFIXLEN) {
-			ecommunity_free(&ecom);
-			vty_out(vty, "%% Invalid prefix length %d\n", plen);
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-	}
-
-	if (add)
-		bgp_rtc_add_static(bgp, (struct ecommunity_val *)ecom->val, plen);
-	else
-		bgp_rtc_remove_static(bgp, (struct ecommunity_val *)ecom->val, plen);
-
-	ecommunity_free(&ecom);
-
-	return CMD_SUCCESS;
-}
-
-void bgp_rtc_add_static(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
-{
+	/* TODO: Move prefix creation from eval into separate function and handle incorrect prefixlens */
 	struct prefix prefix = { 0 };
-	struct bgp_dest *dest;
 	struct bgp_static *bgp_static;
 
 	prefix.family = AF_RTC;
 	prefix.prefixlen = prefixlen;
 	prefix.u.prefix_rtc.origin_as = bgp->as;
-	memcpy(prefix.u.prefix_rtc.route_target, eval, PSIZE(prefixlen) - 4);
-	dest = bgp_node_get(bgp->route[AFI_IP][SAFI_RTC], &prefix);
-
-	if (bgp_dest_has_bgp_path_info_data(dest)) {
-		bgp_dest_unlock_node(dest);
-		return;
+	if (prefixlen >= 32) {
+		memcpy(prefix.u.prefix_rtc.route_target, eval, PSIZE(prefixlen) - 4);
 	}
 
 	bgp_static = bgp_static_new();
-	bgp_static->backdoor = 0;
-	bgp_static->valid = 0;
-	bgp_static->igpmetric = 0;
-	bgp_static->igpnexthop.s_addr = INADDR_ANY;
-	bgp_static->label = MPLS_INVALID_LABEL;
-	bgp_static->label_index = BGP_INVALID_LABEL_INDEX;
-
-	bgp_dest_set_bgp_static_info(dest, bgp_static);
-
-	bgp_static->valid = 1;
 	bgp_static_update(bgp, &prefix, bgp_static, AFI_IP, SAFI_RTC);
 }
 
-void bgp_rtc_remove_static(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
+/* Adaption of bgp_static_update */
+void bgp_rtc_add_dynamic(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
+{
+	struct bgp_dest *dest;
+	struct bgp_path_info *pi;
+	struct bgp_path_info *new;
+	struct attr attr;
+	struct attr *attr_new;
+	struct prefix prefix = { 0 };
+
+	prefix.family = AF_RTC;
+	prefix.prefixlen = prefixlen;
+	prefix.u.prefix_rtc.origin_as = bgp->as;
+	memcpy(prefix.u.prefix_rtc.route_target, eval, PSIZE(prefixlen) - 4);
+	afi_t afi = AFI_IP;
+	safi_t safi = SAFI_RTC;
+
+
+	dest = bgp_afi_node_get(bgp->rib[afi][safi], afi, safi, &prefix, NULL);
+
+	bgp_attr_default_set(&attr, bgp, BGP_ORIGIN_IGP);
+
+	attr.nexthop.s_addr = INADDR_ANY;
+
+	bgp_attr_set_med(&attr, 0);
+
+	attr.mp_nexthop_len = BGP_ATTR_NHLEN_IPV4;
+
+
+	if (bgp_in_graceful_shutdown(bgp))
+		bgp_attr_add_gshut_community(&attr);
+
+	attr_new = bgp_attr_intern(&attr);
+
+	for (pi = bgp_dest_get_bgp_path_info(dest); pi; pi = pi->next)
+		if (pi->peer == bgp->peer_self && pi->type == ZEBRA_ROUTE_BGP
+		    && pi->sub_type == BGP_ROUTE_NORMAL)
+			break;
+
+	if (pi) {
+		bgp_attr_unintern(&attr_new);
+		bgp_dest_unlock_node(dest);
+		return;
+	}
+	/* Make new BGP info. */
+	new = info_make(ZEBRA_ROUTE_BGP, BGP_ROUTE_NORMAL, 0, bgp->peer_self,
+			attr_new, dest);
+
+	bgp_nexthop_reachability_check(afi, safi, new, &prefix, dest, bgp, bgp);
+
+	/* Aggregate address increment. */
+	bgp_aggregate_increment(bgp, &prefix, new, afi, safi);
+
+	/* Register new BGP information. */
+	bgp_path_info_add(dest, new);
+
+	/* route_node_get lock */
+	bgp_dest_unlock_node(dest);
+
+	/* Process change. */
+	bgp_process(bgp, dest, new, afi, safi);
+
+	/* Unintern original. */
+	aspath_unintern(&attr.aspath);
+}
+
+/* Adaption of bgp_static_withdraw */
+static void bgp_rtc_remove_static(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
 {
 	struct prefix prefix = { 0 };
 	struct bgp_dest *dest;
@@ -206,5 +212,88 @@ void bgp_rtc_remove_static(struct bgp *bgp, struct ecommunity_val *eval, uint32_
 
 	bgp_dest_set_bgp_static_info(dest, NULL);
 	bgp_dest_unlock_node(dest);
+}
+
+void bgp_rtc_remove_dynamic(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen)
+{
+	struct bgp_dest *dest;
+	struct bgp_path_info *pi;
+	afi_t afi = AFI_IP;
+	safi_t safi = SAFI_RTC;
+
+	struct prefix prefix = { 0 };
+
+	prefix.family = AF_RTC;
+	prefix.prefixlen = prefixlen;
+	prefix.u.prefix_rtc.origin_as = bgp->as;
+	memcpy(prefix.u.prefix_rtc.route_target, eval, PSIZE(prefixlen) - 4);
+
+	dest = bgp_afi_node_get(bgp->rib[afi][safi], afi, safi, &prefix, NULL);
+
+	/* Check selected route and self inserted route. */
+	for (pi = bgp_dest_get_bgp_path_info(dest); pi; pi = pi->next)
+		if (pi->peer == bgp->peer_self && pi->type == ZEBRA_ROUTE_BGP
+		    && pi->sub_type == BGP_ROUTE_NORMAL)
+			break;
+
+	/* Withdraw static BGP route from routing table. */
+	if (pi) {
+		SET_FLAG(pi->flags, BGP_PATH_UNSORTED);
+		bgp_aggregate_decrement(bgp, &prefix, pi, afi, safi);
+		bgp_unlink_nexthop(pi);
+		bgp_path_info_delete(dest, pi);
+		bgp_process(bgp, dest, pi, afi, safi);
+	}
+
+	/* Unlock bgp_node_lookup. */
 	bgp_dest_unlock_node(dest);
+}
+
+int bgp_rtc_static_from_str(struct vty *vty, struct bgp *bgp, const char *str, bool add)
+{
+	struct ecommunity *ecom = NULL;
+	int plen = BGP_RTC_MAX_PREFIXLEN;
+	char *pnt;
+	char *cp;
+
+	/* Find slash inside string. */
+	pnt = strchr(str, '/');
+
+	/* String doesn't contain slash. */
+	if (pnt == NULL) {
+		ecom = ecommunity_str2com(str, ECOMMUNITY_ROUTE_TARGET, 0);
+		if (ecom == NULL) {
+			vty_out(vty, "%% Can't parse ecommunity %s\n", str);
+			return CMD_WARNING_CONFIG_FAILED;
+		}
+	} else {
+		plen = (uint8_t)atoi(++pnt);
+		cp = XMALLOC(MTYPE_TMP, (pnt - str) + 1);
+		memcpy(cp, str, pnt - str -1);
+		*(cp + (pnt - str)  -1) = '\0';
+		ecom = ecommunity_str2com(cp, ECOMMUNITY_ROUTE_TARGET, 0);
+
+		XFREE(MTYPE_TMP, cp);
+
+		if (ecom == NULL) {
+			vty_out(vty, "%% Can't parse ecommunity %s\n", str);
+			return CMD_WARNING_CONFIG_FAILED;
+		}
+
+		/* Get prefix length. */
+		if (plen!= 0 && (plen < 32 ||  plen > BGP_RTC_MAX_PREFIXLEN)) {
+			ecommunity_free(&ecom);
+			vty_out(vty, "%% Invalid prefix length %d\n", plen);
+			return CMD_WARNING_CONFIG_FAILED;
+		}
+	}
+
+	if (add)
+		bgp_rtc_add_static(bgp, (struct ecommunity_val *)ecom->val, plen);
+	else
+		bgp_rtc_remove_static(bgp, (struct ecommunity_val *)ecom->val, plen);
+
+	ecommunity_free(&ecom);
+
+	return CMD_SUCCESS;
 }
