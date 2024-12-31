7ed40dea57e3a3648a936d407a2cc94c5b6b872e
Alexander Sohn <github@asohn.de>
2024-12-31 11:03:59 +0100

    fixup bgpd: Generate rtc prefixes from vni route-targets
    Signed-off-by: Alexander Sohn <github@asohn.de>

--- bgpd/bgp_rtc.h
+++ bgpd/bgp_rtc.h
@@ -11,12 +11,15 @@
 #include "bgpd.h"
 #include "bgp_attr.h"
 #include "vty.h"
+#include "bgp_nht.h"
 
 #define BGP_RTC_MAX_PREFIXLEN 96
 
 extern int bgp_nlri_parse_rtc(struct peer *peer, struct attr *attr, struct bgp_nlri *packet,
 			      bool withdraw);
 extern int bgp_rtc_filter(struct peer *peer, struct ecommunity *ecom, bool show_command);
+extern void bgp_rtc_add_dynamic(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen);
+extern void bgp_rtc_remove_dynamic(struct bgp *bgp, struct ecommunity_val *eval, uint32_t prefixlen);
 extern int bgp_rtc_static_from_str(struct vty *vty, struct bgp *bgp, const char *str, bool add);
 
 extern char *bgp_rtc_prefix_display(char *buf, size_t size, uint16_t prefix_len,
