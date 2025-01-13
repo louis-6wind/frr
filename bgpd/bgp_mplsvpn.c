90e20ef68e682b2e5ed6f39395c3b4a57e880275
Louis Scalbert <louis.scalbert@6wind.com>
2025-01-13 18:07:37 +0100

    WIP announce from VPN
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -1186,7 +1186,7 @@ leak_update(struct bgp *to_bgp, struct bgp_dest *bn,
 		}
 
 		if (attrhash_cmp(bpi->attr, new_attr) && labelssame
-		    && !CHECK_FLAG(bpi->flags, BGP_PATH_REMOVED)) {
+		    && !CHECK_FLAG(bpi->flags, BGP_PATH_REMOVED) && !CHECK_FLAG(bpi->flags, BGP_PATH_ATTR_CHANGED)) {
 
 			bgp_attr_unintern(&new_attr);
 			if (debug)
