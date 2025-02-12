442dc53e88d410edf292cd87b926df056e80dc7d
Louis Scalbert <louis.scalbert@6wind.com>
2025-02-12 14:44:07 +0100

    debug logs
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -2537,6 +2537,8 @@ static int bgp_update_receive(struct peer_connection *connection,
 	 * and MP EoR should have only an empty MP_UNREACH
 	 */
 	if (!update_len && !withdraw_len && nlris[NLRI_MP_UPDATE].length == 0) {
+		zlog_info("%s: rcvd End-of-RIB for %pBP ???", __func__, peer);
+
 		afi_t afi = 0;
 		safi_t safi;
 		struct graceful_restart_info *gr_info;
@@ -2616,6 +2618,8 @@ static int bgp_update_receive(struct peer_connection *connection,
 				 * prefixes are properly withdrawn.
 				 */
 				SET_FLAG(peer->flags, PEER_FLAG_RTC_UPDATE);
+				zlog_debug("DEBFLAG %pBP set PEER_FLAG_RTC_UPDATE %s", peer,
+					   __func__);
 				bgp_add_rtc_eor_mark(peer->bgp);
 			}
 
